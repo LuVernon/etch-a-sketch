fd2f6188266f82f0dada37d314e5c9590cc20584
LuVernon <lucasvernon@icloud.com>
2023-11-01 01:22:19 -0700

    User cannot enter a valid negative number through the window prompt. Clicking on the button to redraw the canvas is now possible and will clear and redraw using new dimensions.

--- script.js
+++ script.js
@@ -4,18 +4,21 @@ let button = document.querySelector("#ask");
 function formate (size) {
     let ratios = 960/size;  
     let div = document.createElement("div");
+    if (grid.childNodes.length > 0) {
+            grid.replaceChildren();
+    }
     div.style.height = ratios + "px"; 
     div.style.width = ratios + "px"; 
     div.style.flex = ratios + "px"; 
     div.classList.add('child');
-    for (i=0; i<size*size; i++) {
+    for (let i=0; i<size*size; i++) {
        grid.appendChild(div.cloneNode(true));
     }
 };
 
 button.addEventListener("click", () => {
     let customSize = window.prompt("How many squares do you want per side?");
-    if (customSize >= 101 || isNaN(customSize)) {
+    if (customSize >= 101 || isNaN(customSize) || customSize < 1) {
         window.alert("Please pick a number below from 1-100.");
         return;
     };

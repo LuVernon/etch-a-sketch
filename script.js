d811cbe3b33c20cef1472de8492a543bc7e3d207
LuVernon <lucasvernon@icloud.com>
2023-11-01 00:41:21 -0700

    Added custom dimensions button to give the user the option to form their own sized canvas of divs. Will create the display using that user generated number for the side lengths.

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 let grid = document.querySelector("#gridder");
+let button = document.querySelector("#ask");
 
-function formate () {
+function formate (size) {
+    console.log(size);
     let div = document.createElement("div");
     div.classList.add('child');
     for (i=0; i<256; i++) {
@@ -8,4 +10,12 @@ function formate () {
     }
 };
 
-formate();
+button.addEventListener("click", () => {
+    let customSize = window.prompt("How many tiles do you want for your painting!?");
+    console.log(typeof customSize);
+    if (typeof customSize != "number") {
+        console.log("hi");
+        return;
+    }
+    formate(customSize);
+});

b80395aab9a795baf0ecc870b6d595fe967707f5
LuVernon <lucasvernon@icloud.com>
2023-10-31 20:40:40 -0700

    Flexbox of divs, arranged in a grid formation created via javascript. On hover the background color of each div will change to transparent.

--- script.js
+++ script.js
@@ -3,10 +3,8 @@ let grid = document.querySelector("#gridder");
 function formate () {
     let div = document.createElement("div");
     div.classList.add('child');
-    for (i=0; i<16; i++) {
+    for (i=0; i<256; i++) {
        grid.appendChild(div.cloneNode(true));
-       
-       
     }
 };
 
